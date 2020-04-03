3739f705b294740b39ee07af6ce7199dfd88bcbb
Akin Delamarre <adelamarre@pelmorex.com>
2020-04-02 23:33:20 -0400

    Removing console log

--- src/Advertising.js
+++ src/Advertising.js
@@ -76,7 +76,7 @@ export default class Advertising {
                 }
             })
         );
-        console.log('DEBUG', '%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%');
+
         Advertising[queueForAmazon]('fetchBids', [
             {
                 slots: [
