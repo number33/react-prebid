8c39210412a99bc03caddea868a8a89a337976d4
Akin Delamarre <adelamarre@pelmorex.com>
2020-04-06 16:39:32 -0400

    Revert "Reverting my changes."
    This reverts commit 74326b3e2f44470fd1844abf241491e7d5f7fa14.

--- src/Advertising.js
+++ src/Advertising.js
@@ -19,6 +19,12 @@ const queueForPrebid = Symbol('queue for Prebid (private method)');
 const setDefaultConfig = Symbol('set default config (private method)');
 const executePlugins = Symbol('execute plugins (private method)');
 
+const setupAdCallSync = Symbol("a");
+const scriptCmd = Symbol("b");
+const withAmaQueue = Symbol('with Amazon queue (private method)');
+const queueForAmazon = Symbol('queue for Amazon (private method)');
+
+
 export default class Advertising {
     constructor(config, plugins = [], onError = () => {}) {
         this.config = config;
@@ -44,7 +50,8 @@ export default class Advertising {
         this[setupCustomEvents]();
         await Promise.all([
             Advertising[queueForPrebid](this[setupPrebid].bind(this), this.onError),
-            Advertising[queueForGPT](this[setupGpt].bind(this), this.onError)
+            Advertising[queueForGPT](this[setupGpt].bind(this), this.onError),
+            Advertising[scriptCmd](this[setupAdCallSync].bind(this))
         ]);
         if (queue.length === 0) {
             return;
@@ -101,11 +108,43 @@ export default class Advertising {
                     adUnitCodes: [id],
                     bidsBackHandler() {
                         window.pbjs.setTargetingForGPTAsync([id]);
-                        Advertising[queueForGPT](() => window.googletag.pubads().refresh([slots[id]]), this.onError);
+                        // Advertising[queueForGPT](() => window.googletag.pubads().refresh([slots[id]]), this.onError);
+
+                        Advertising[scriptCmd](() => window.adCallSyncList[id].prebidBidRequest = true);
+                        Advertising[queueForGPT](() => {
+                          if (window.adCallSyncList[id].amazonBidRequest && window.adCallSyncList[id].prebidBidRequest && !window.adCallSyncList[id].adRequestSent) {
+                            window.googletag.pubads().refresh([slots[id]], this.onError);
+                            window.adCallSyncList[id].adRequestSent = true;
+                          }
+                        });
                     }
                 }),
             this.onError
         );
+
+        Advertising[queueForAmazon]('fetchBids', [
+          {
+            slots: [
+              {
+                  slotID: 'div-gpt-ad-topbanner',
+                  slotName: '/19849159/web-theweathernetwork.com/homepage/div-gpt-ad-topbanner',
+                  sizes: [[300, 50], [320, 50]]
+                }
+              ]
+            },
+            function(bids) {
+              window.googletag.cmd.push(function() {
+                window.apstag.setDisplayBids();
+                Advertising[scriptCmd](() => window.adCallSyncList[id].amazonBidRequest = true);
+                Advertising[queueForGPT](() => {
+                  if (window.adCallSyncList[id].amazonBidRequest && window.adCallSyncList[id].prebidBidRequest && !window.adCallSyncList[id].adRequestSent) {
+                    window.googletag.pubads().refresh([slots[id]]);
+                    window.adCallSyncList[id].adRequestSent = true;
+                  }
+                });
+              });
+            }
+          ]);
     }
 
     isConfigReady() {
@@ -118,6 +157,17 @@ export default class Advertising {
     }
 
     // ---------- PRIVATE METHODS ----------
+
+    [setupAdCallSync]() {
+      window.adCallSyncList = {};
+      this.config.slots.forEach((slot) => {
+        window.adCallSyncList[slot.id] = {
+          prebidBidRequest: false,
+          amazonBidRequest: false,
+          adRequestSent: false
+        };
+      });
+    }
 
     [setupCustomEvents]() {
         if (!this.config.customEvents) {
@@ -297,5 +347,29 @@ export default class Advertising {
                 }
             })
         );
+    }
+
+    static [queueForAmazon](key, param) {
+      if (key === 'init') {
+        return Advertising[withAmaQueue](window.apstag.init, param);
+      } else {
+        return Advertising[withAmaQueue](window.apstag.fetchBids, param);
+      }
+    }
+
+
+    static [withAmaQueue](queue, param) {
+      return new Promise(resolve => {
+          console.log("AD DEBUG", param[0]);
+          queue(...param);
+          resolve();
+      });
+    }
+
+    static [scriptCmd](func) {
+      return new Promise(resolve => {
+        func()
+        resolve();
+      });
     }
 }
